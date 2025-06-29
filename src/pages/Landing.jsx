951673f39ad51e5ee245d260b60a854ca7fd58d2
KarthikVarma19 <ganarajukarthikvarma1635@gmail.com>
2025-06-30 00:18:25 +0530

    fix Landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React, { useState } from "react";
+import { useState } from "react";
 
 import {
   Accordion,
@@ -11,7 +11,7 @@ import {
 import { useNavigate } from "react-router-dom";
 
 const Landing = () => {
-  const [longUrl, setLongUrl] = useState();
+  const [longUrl, setLongUrl] = useState("");
   const navigate = useNavigate();
 
   const handleShorten = (e) => {
