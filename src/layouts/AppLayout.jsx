21f33acc6ca49bd27b4df41f669a8e1493421136
KarthikVarma19 <ganarajukarthikvarma1635@gmail.com>
2025-06-30 10:35:00 +0530

    change footer

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,4 +1,4 @@
-import Header from "@/components/header";
+import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
@@ -8,7 +8,7 @@ const AppLayout = () => {
         <Header />
         <Outlet />
       </main>
-      <div className="p-10 text-center bg-gray-800 mt-10 w-full">
+      <div className="p-5 text-center bg-gray-800 mt-10 w-full">
         Made with 💗 by Karthik Varma
       </div>
     </div>
